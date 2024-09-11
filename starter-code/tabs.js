62ca6bbb06c12d15c24695e6f5bc386e269b5533
Samuel <goodlifesam@gmail.com>
2024-09-11 11:26:35 -0700

    added a ton

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -17,22 +17,20 @@ function changeTabFocus(e) {
         tabs[tabFocus].setAttribute("tabindex", -1);
     }
     
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
         }
-    }
     
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-        }
-    }
-    
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
 }
 
 
@@ -43,18 +41,29 @@ function changeTabPanel(e) {
     
     const tabContainer = targetTab.parentNode;
     const mainContainer = tabContainer.parentNode;
+
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
+
+    targetTab.setAttribute("aria-selected", true);
+
+    hideContent(mainContainer, '[role="tabpanel"]')
+    showContent(mainContainer, [`#${targetPanel}`])
+
+
+    hideContent(mainContainer, 'picture')
+    showContent(mainContainer, [`#${targetImage}`])
     
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach((panel) => panel.setAttribute("hidden", true));
-    
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
-    
-    mainContainer
-        .querySelectorAll('picture')
-        .forEach((picture) => picture.setAttribute("hidden", true));
-        
-    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
-    
-    // console.log(mainContainer);
+}
+
+function hideContent(parent, content){
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content){
+    parent.querySelector(content).removeAttribute('hidden');
+
 }
